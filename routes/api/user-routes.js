014ccf91df463bdc0aac6ce8e0cd715fe680bae2
RayFlaherty <90649331+RayFlaherty@users.noreply.github.com>
2022-01-18 16:38:11 -0600

    debugged some stuff

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -6,18 +6,8 @@ router.get('/', (req, res) => {
   // Access our User model and run .findAll() method)
   User.findAll({
     attributes: {exclude: ['password']},
-    where : {
-      id: req.params.id
-    }
   })
-    .then(dbUserData =>{
-      if (!dbUserData) {
-        res.status (404).json({message: 'No user found with this id'});
-        return;
-      }
-
-      res.json(dbUserData);
-    }) 
+    .then(dbUserData => res.json(dbUserData))
     .catch(err => {
       console.log(err);
       res.status(500).json(err);
@@ -26,6 +16,7 @@ router.get('/', (req, res) => {
 // GET /api/users/1
 router.get('/:id', (req, res) => {
   User.findOne({
+    attributes: { exclude: ['password'] },
     where: {
       id: req.params.id
     }
