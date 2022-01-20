c4044441bb808dfe6af03cd83bb1b94133a3bfe7
RayFlaherty <90649331+RayFlaherty@users.noreply.github.com>
2022-01-19 18:12:01 -0600

    adding

--- models/index.js
+++ models/index.js
@@ -1,6 +1,16 @@
 const User = require('./User');
 const Post = require('./Post');
+const Vote = require('./Vote');
+const Comment = require('./Comment');
 // create associations
+Vote.belongsTo(User, {
+  foreignKey: 'user_id'
+});
+
+Vote.belongsTo(Post, {
+  foreignKey: 'post_id'
+});
+
 User.hasMany(Post, {
   foreignKey: 'user_id'
 });
@@ -9,5 +19,35 @@ Post.belongsTo(User, {
   foreignKey: 'user_id',
 });
 
+User.belongsToMany(Post, {
+  through: Vote,
+  as: 'voted_posts',
+  foreignKey: 'user_id'
+});
 
-module.exports = { User, Post };
+Post.belongsToMany(User, {
+  through: Vote,
+  as: 'voted_posts',
+  foreignKey: 'post_id'
+});
+
+Comment.belongsTo(User, {
+  foreignKey: 'user_id'
+});
+
+Comment.belongsTo(Post, {
+  foreignKey: 'post_id'
+});
+
+User.hasMany(Comment, {
+  foreignKey: 'user_id'
+});
+
+Post.hasMany(Comment, {
+  foreignKey: 'post_id'
+});
+
+
+
+
+module.exports = { User, Post, Vote, Comment };
